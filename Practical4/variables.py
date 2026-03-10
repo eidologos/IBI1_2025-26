6986a890efff8b19fb8cd2b035af2ed16eb91898
张含萌 <zhanghanmeng@zhanghanmengdeMacBook-Pro.local>
2026-03-10 10:57:11 +0800

    commit changes

---
+++ Practical4/variables.py
@@ -0,0 +1,23 @@
+#population in 2004(10000 people)
+a=508
+#population in 2014(10000 people))
+b=533
+#population in 2024(10000 people))
+c=555
+#change from 2004-2014
+d=b-a
+#change from 2014-2024
+e=c-b
+# Print the population change values
+print("Population change 2004-2014: ", d)
+print("Population change 2014-2024: ", e)
+# Compare d and e, judge the population growth trend
+if d > e:
+    print("population growth in Scotland is decelerating")
+elif e > d:
+    print("population growth in Scotland is accelerating")
+else:
+    print("the speed of population growth remains unchanged")
+
+# Conclusion: d=25, e=22 
+# d>e, population growth is decelerating
