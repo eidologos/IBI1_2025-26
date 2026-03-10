ade486c29b42c1de9dd3f6177d651d53aa3773ae
张含萌 <zhanghanmeng@zhanghanmengdeMacBook-Pro.local>
2026-03-10 20:24:59 +0800

    explain the mystery code

--- Practical4/mystery_code.py
+++ Practical4/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer:
+# Answer:This code generates 11 random numbers between 1 and 9,sums them up and print the final value
 
 # Import libraries
 # randint allows drawing a random number,
@@ -10,12 +10,12 @@ from random import randint
 # e.g. ceil(4.2)=5
 from math import ceil
 
-total_rand = 0
-progress=0
-while progress<=10:
-	progress+=1
-	n = randint(1,10)
-	total_rand+=n
+total_rand = 0 #record the sum value of random numbers
+progress=0 #control the number of random numbers
+while progress<=10: 
+	progress+=1 #ensure that 11 random numbers will be generated
+	n = randint(1,10) #generate a random number between 1 and 9
+	total_rand+=n #add the random number of this round to the total sum
 
 print(total_rand)
 
